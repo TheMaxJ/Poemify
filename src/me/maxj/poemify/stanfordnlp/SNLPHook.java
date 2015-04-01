4b76d1c51d9a9b8261e1a0a4deda96c441e8990c
TheMaxJ <maxjohnson@Maxs-MacBook-Air-3.local>
2015-04-01 12:33:49 -0700

    Created Grammatical Analyzer

--- src/me/maxj/poemify/stanfordnlp/SNLPHook.java
+++ src/me/maxj/poemify/stanfordnlp/SNLPHook.java
@@ -1,5 +1,34 @@
 package me.maxj.poemify.stanfordnlp;
 
+import java.util.Properties;
+
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.semgraph.SemanticGraph;
+import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.BasicDependenciesAnnotation;
+import edu.stanford.nlp.util.CoreMap;
+
 public class SNLPHook {
+	public static StanfordCoreNLP pipeline;
+	
+	static {
+		Properties props = new Properties();
+		props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref, sentiment");
+		pipeline = new StanfordCoreNLP(props);	
+	}
+	
+	public static void main(String[] args) {
+		// Test Stuff Goes here
+	}
+	
+	public static SemanticGraph getSemanticGraph(String sentence) {
+		Annotation document = new Annotation(sentence);
+		pipeline.annotate(document);
+		for (CoreMap map : document.get(SentencesAnnotation.class)) {
+			return map.get(BasicDependenciesAnnotation.class);
+		}
+		return null;
+	}
 
 }
